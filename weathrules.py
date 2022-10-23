b6fec091aa23600486a7e2a32a3ae8f253f64282
Anujap04 <116511972+Anujap04@users.noreply.github.com>
2022-10-23 23:00:47 +0530

    Create weathrules.py

---
+++ weathrules.py
@@ -0,0 +1,4 @@
+print("The rules for this app are pretty simple")
+print("Only one location can be entered at a time")
+print("Result temp shown should be shown inacurate sometimes")
+print("Use this application for personal use")
